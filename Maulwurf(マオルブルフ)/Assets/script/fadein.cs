e46f71f0c26210df9967002f7cf626284558b680
fox <i>
2019-04-09 20:42:48 +0900

    start

---
+++ Maulwurf(マオルブルフ)/Assets/script/fadein.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class fadein : MonoBehaviour
+{
+    float fadeinspeed = 0.01f;
+    float red, green, blue, alfa;
+    bool isfadein = true;
+    Image fadeinimage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeinimage = GetComponent<Image>();
+        red = fadeinimage.color.r;
+        green = fadeinimage.color.g;
+        blue = fadeinimage.color.b;
+        alfa = fadeinimage.color.a;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isfadein)
+        {
+            Startfadein();
+        }
+    }
+
+    void Startfadein()
+    {
+        fadeinimage.enabled = true;
+        alfa -= fadeinspeed;
+        setalfa();
+        if (alfa == 0) isfadein = false;
+    }
+    void setalfa()
+    {
+        fadeinimage.color = new Color(red, green, blue, alfa);
+    }
+}
---
+++ Maulwurf(マオルブルフ)/Assets/script/fadeout.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class fadeout : MonoBehaviour
+{
+    float fadeoutspeed = 0.01f;//透明度を変えるスピード
+    float red, green, blue, alfa;//パネルの色、不透明度を管理
+    public static bool isfadeout = false;
+    Image fadeimage;//透明度を変更するパネルのイメージ
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeimage = GetComponent<Image>();
+        red = fadeimage.color.r;
+        green = fadeimage.color.g;
+        blue = fadeimage.color.b;
+        alfa = fadeimage.color.a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey) isfadeout = true;
+        if (isfadeout==false) GetComponent<AudioSource>().Play();
+        if (isfadeout)
+        {
+            Startfadeout();
+            Invoke("changegame", 2f);
+        }
+
+    }
+
+    void Startfadeout()
+    {
+        
+        fadeimage.enabled = true;//パネルの表示をon
+        alfa += fadeoutspeed;
+        setalfa();
+        if (alfa >= 1) isfadeout = false;
+
+    }
+    void setalfa()
+    {
+        fadeimage.color = new Color(red, green, blue, alfa);
+    }
+    void changegame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
